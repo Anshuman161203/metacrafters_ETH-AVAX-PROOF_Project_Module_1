6787c9679256a314befe9c8a0cf62b8b27f4533c
ANSHUMAN ROSHAN <anshumanroshan1607@gmail.com>
2024-07-03 23:16:25 +0530

    Update Functions and Errors.sol

--- Functions and Errors.sol
+++ Functions and Errors.sol
@@ -1,27 +1,26 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity ^0.8.18;
 
-contract error{
-    address public owner;
-    uint public count=0;
-    constructor(){
-        owner=msg.sender;
+contract uid_Checker {
+    uint public studentCount = 0;
+
+    // Function to assert that the UID is within the valid range
+    function assertUID(uint uid) public pure {
+        assert(uid >= 1 && uid <= 99);
     }
-    function Require()   public {
-        count++;
-        require(msg.sender==owner,"caller is not Owner");
-    }
-    function Assert()   public {
-        count++;
-        assert(msg.sender==owner);
-    }
-    function Revert()   public {
-        count++;
-        if(msg.sender!=owner){
-            revert("Requires Owner");
+
+    // Function to increment the student counter if the UID is valid
+    function incrementStudentCounter(uint uid) public returns (uint) {
+        if (uid < 1 || uid > 99) {
+            revert("This is not a valid UID");
         }
-        else if(count>3){
-            revert("Count bigger than 3");
-        }
+        studentCount += 1;
+        return studentCount;
+    }
+
+    // Function to require that the UID is within the valid range
+    function checkUID(uint uid) public pure returns (bool) {
+        require(uid >= 1 && uid <= 99, "UID range should be between 1 and 99");
+        return true;
     }
 }
